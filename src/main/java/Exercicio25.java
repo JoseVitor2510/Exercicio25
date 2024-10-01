18167cb0bfd285890379414cb5abf07909419664
José Vitor Teófilo da Silva <josevitorteofilosl@gmail.com>
2024-10-01 16:32:42 -0300

    Update Exercicio25.java

--- src/main/java/Exercicio25.java
+++ src/main/java/Exercicio25.java
@@ -1,24 +1,30 @@
-
 import javax.swing.JOptionPane;
-
 
 public class Exercicio25 {
 
     public static void main(String[] args) {
-        int qtde=20;
-        int vetor[] = new int[qtde];
-        for (int i = 0;i < qtde;i++){
-        vetor[i] = Integer.parseInt(JOptionPane.showInputDialog("Digite o número :" + i ));
-        }
-        int soma=0;
-        for (int i = 0;i < qtde;i++){
-        soma = soma + vetor[i];
-        }
-        double media = soma/qtde;
-        JOptionPane.showMessageDialog(null,"A soma é: " + soma + "\nA média é " + media);
-        }
-    }
 
-        
-   
+        //Declaração de variáveis
+
+        int qtde = 20;
+        int[] vetor = new int[qtde];
+
+        // Entrada de dados
+        for (int i = 0; i < qtde; i++) {
+            vetor[i] = Integer.parseInt(JOptionPane.showInputDialog("Digite o número: " + i));
+        }
+
+        // Cálculo da soma
+        int soma = 0;
+        for (int i = 0; i < qtde; i++) {
+            soma += vetor[i];
+        }
+
+        // Cálculo da média
+        double media = (double) soma / qtde;
+
+        // Exibição dos resultados
+        JOptionPane.showMessageDialog(null, "A soma é: " + soma + "\nA média é: " + media);
+    }
+}
 
